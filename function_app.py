8e07caebb33d36a9411fcdb3664de0eb2fb4add8
Rapha-Cruz <raphajcruz@hotmail.com>
2024-05-28 17:02:34 -0300

    quarta

--- function_app.py
+++ function_app.py
@@ -24,7 +24,7 @@ def function_app(req: func.HttpRequest) -> func.HttpResponse:
              status_code=200
         )
 
-@app.route(route="function_app2", auth_level=func.AuthLevel.FUNCTION)
+@app.route(route="/function_app2", auth_level=func.AuthLevel.FUNCTION)
 def function_app2(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
